f7a5c8982dd3d23028d49bd445e50a7f075b7871
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-18 23:31:19 -0500

    creating schema for our mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,4 +31,10 @@ const typeDefs = gql`
   type Query {
     me: User
   }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
 `;
