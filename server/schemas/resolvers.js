d7b39434822fe48c7162a9e34e467c4f0df700c6
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-18 22:52:26 -0500

    applying proper code to addUser function within the mutation object

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,6 +14,10 @@ const resolvers = {
   },
   Mutation: {
     addUser: async (parent, { email, password }) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
 
     },
     login: async (parent, { email, password }) => {
