248f4bde2ccd289875fd830609508ed0cb7df337
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-18 23:20:46 -0500

    creating gql schema for Book

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,11 +2,18 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
+    _id: ID!
     username: String!
-    email: String!
+    email: String
     bookCount: Int
     savedBooks: [Book]
   }
-  
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
 `;
