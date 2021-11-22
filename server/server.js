9e5cf70268c633206bffc3ca3b61b8fbfe816c93
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-21 19:40:27 -0500

    changing home route

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ async function startApolloServer(typeDefs, resolvers) {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
 
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
 
