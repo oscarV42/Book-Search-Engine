e982675fd5a26afc47d2999b9ed1f46c8f1e10b2
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-23 19:24:26 -0500

    updating gitignore

---
+++ client/build/precache-manifest.34a5af576a5de79abff8f681802ace71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c210bda7c011a67c015f7b6e23bbc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70ea3bcd35fd1217db06",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "efbf13afbf613e733030",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "70ea3bcd35fd1217db06",
+    "url": "/static/js/2.52c91ad2.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.52c91ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efbf13afbf613e733030",
+    "url": "/static/js/main.fc29d568.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
